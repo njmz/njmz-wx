8e6082f3d7bfc0f35b68465b4f273ea33afb0656
favous <favous@163.com>
2017-06-09 17:49:05 +0800

    njmz项目搭建

---
+++ wx-service/src/main/java/com/njmz/wx/service/App.java
@@ -0,0 +1,13 @@
+package com.njmz.wx.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
